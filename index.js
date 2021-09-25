6f6d12b6540d6429996e3abaf2ed8d46ac994ccc
gabrieleaquaro <gabriele.aquaro@gmail.com>
2021-09-25 18:11:55 +0200

    work, not finished

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const { app, BrowserWindow } = require("electron");
 const url = require("url");
 
 function newApp() {
-  win = new BrowserWindow();
+  win = new BrowserWindow({icon:'./images/icon.png',
+                           webPreferences: {
+                              nodeIntegration: true,
+                              contextIsolation: false,
+                          }
+                        });
   win.loadURL(
     url.format({
       pathname: "index.html",
       slashes: true
-    })
+    })    
   );
-
+  //report is a JSON dictionary containing all the current data 
+  report = JSON.parse("{}")
+  report["creation_date"] = new Date().getDate() + "_" + new Date().getMonth();
 }
 
 app.on("ready", newApp);
