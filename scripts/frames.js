56908a60b6d9fc437e88acac7734dc18bac98f26
CarrattaFrancesco <francesco.carratta@mail.polimi.it>
2021-09-26 11:47:23 +0200

    Revert "Frame larghezza intera e collegato al menu con tutte le relative pagine"
    This reverts commit fa104cf848481f6c66374c530ec29a4be949327c.

--- scripts/frames.js
+++ scripts/frames.js
@@ -1,20 +1,3 @@
-var previousMenuElementActive = document.getElementById("homeIndex")
-
 function resizeIframe(obj) {
     obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
-}
-
-function changeFrame(element,src){
-    console.log("Change frame page")
-    var iFrame = document.getElementById("frame")
-    iFrame.src = src
-
-    // cambio colorazione degli indici del menù
-    if(!previousMenuElementActive.isEqualNode(element)){
-        console.log("Cambio active")
-        previousMenuElementActive.classList.remove("active")
-        element.classList.add("active")
-        previousMenuElementActive = element
-    }
-
 }
