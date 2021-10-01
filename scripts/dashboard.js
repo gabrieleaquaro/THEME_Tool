6e6a35e343e192c091112ffdc99eb1c5a7bafc86
gabrieleaquaro <gabriele.aquaro@gmail.com>
2021-10-01 20:48:14 +0200

    graph colors

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -91,16 +91,56 @@ function generateChart_PSF(values){
 
     var max_present_value = Math.max(...values_list);
     var limit_max = max_present_value < 5 ? 2 :(max_present_value < 10 ? 5 : (max_present_value < 20 ? 15 : (max_present_value < 50 ? 30 : (max_present_value < 100 ? 60 : 100)))); 
-    var stepSize = limit_max < 3 ? 0.5 : limit_max < limit_max <= 5 ? 1 : (limit_max < 10 ? 2 :(limit_max < 20 ? 5 : 10));              
+    var stepSize = limit_max < 3 ? 0.5 : limit_max <  5 ? 1 : (limit_max < 10 ? 2 :(limit_max < 20 ? 5 : 15));            
     const data = {
         labels: labels,
         datasets: [
+            // {   
+            //     data: [100,100,100,100,100,100,100,100],
+            //     backgroundColor : '#e3191920',
+            //     borderColor :'#e3191920',
+            //     pointRadius : 0,
+            //     order: 4,
+            //     clip:0
+            // },
+            // {   
+            //     data: [10,10,10,10,10,10,10,10],
+            //     backgroundColor : '#ede60e30',
+            //     borderColor :'#ede60e30',
+            //     pointRadius : 0,
+            //     order: 3,
+            //     clip:0
+            // },
+            // {   
+            //     data: [5,5,5,5,5,5,5,5],
+            //     backgroundColor : '#048f2040',
+            //     borderColor :'#048f2040',
+            //     pointRadius : 0,
+            //     order: 3,
+            //     clip:0
+            // },
+            // {   
+            //     data: [1,1,1,1,1,1,1,1],
+            //     backgroundColor : '#34eb4950',
+            //     borderColor :'#34eb4950',
+            //     pointRadius : 0,
+            //     order:2,
+            //     clip:0
+            // },
             {
+                label: 'Valore',
                 data: values_list,
                 borderColor: '#000',
-                pointBackgroundColor: '#000', 
-                borderDash: [2],
+                backgroundColor: '#a6a6a660',
+                pointBackgroundColor: function(context){
+                    var index = context.dataIndex;
+                    var value = context.dataset.data[index];
+                    return value <= 1 ? '#34eb49' : value <= 5 ? '#e9ed66' : '#e31919';
+                }, 
+                pointRadius: 4,
+                order:1,
             },
+            
         ]
     };
 
