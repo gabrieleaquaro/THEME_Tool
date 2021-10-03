4289c4b7d9ba73c735eea2915bc2179641cd7aac
CarrattaFrancesco <francesco.carratta@mail.polimi.it>
2021-10-03 16:33:19 +0200

    aggiunti barriere dirette a risultati, iniziata funzione grafico

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -122,14 +122,34 @@ function updateRisultati_results(report){
             }
         })
         generateChart_PSF(report["prob_errore"]);
-    }
-    else{
+    }else{
         generateChart_PSF({});
         elements_PSF = ["TempoDisponibie","StressDaMinaccia","ComplessitàTask","Esperienza" ,"Procedure" ,"InterazioneUmanoMacchina","ContestoAmbientale","Affaticamento"]
         elements_PSF.forEach(function(key){
             document.getElementById(key).innerText = '-';
         });
     }
+
+    if(report['barriere_dirette']){
+        //First table of PSF
+        elements_barriereDirette = ["PrestSicuraCompiti","Adesione","PrestSicuraContesto","Partecipazione" ,"LavoroSquad" ,"Comunicazione"]
+        elements_barriereDirette.forEach(function(key){
+            if(report['barriere_dirette'][key] != null){
+                document.getElementById(key).innerText = report['barriere_dirette'][key];
+            }
+            else{
+                document.getElementById(key).innerText = '-';
+            }
+        })
+        //generateChart_PSF(report["prob_errore"]);
+    }else{
+        //generateChart_PSF({});
+        elements_barriereDirette = ["PrestSicuraCompiti","Adesione","PrestSicuraContesto","Partecipazione" ,"LavoroSquad" ,"Comunicazione"]
+        elements_barriereDirette.forEach(function(key){
+            document.getElementById(key).innerText = '-';
+        });
+    }
+
 }
 
 //GENERATE THE CHART OF PSF IN RESUTS PAGE
@@ -147,6 +167,184 @@ function generateChart_PSF(values){
     var values_list =  [values["TempoDisponibie"],values["StressDaMinaccia"],values["ComplessitàTask"],
                         values["Esperienza"],values["Procedure"],values["InterazioneUmanoMacchina"],
                         values["ContestoAmbientale"],values["Affaticamento"]]; 
+
+    
+    //To rescale the values that makes no sense elseway on the graph
+    const values_map = {
+        0.1   : 1,
+        1     : 2,
+        '1.0' : 2,
+        2     : 3,
+        5     : 4,
+        10    : 5,
+        15    : 6,
+        20    : 7,
+        50    : 8,
+        100   : 9
+    } 
+    var chart_data = values_list.map(x => values_map[x]);
+    var max_value = Math.max(...chart_data);
+
+    //Datasets for the chart
+    const data = {
+        labels: chart_labels,
+        datasets: [
+            {   
+                lable: 'red',
+                data: [8,8,8,8,8,8,8,8],
+                backgroundColor : '#FFFFFF00',
+                borderColor :'#FF3333',
+                pointRadius : 0,
+                order: 5,
+                hoverRadius : 0,
+                hitRadius: 0,
+                
+            },
+            {   
+                label: 'orange',
+                data: [6,6,6,6,6,6,6,6],
+                backgroundColor : '#FFFFFF00',
+                borderColor :'#ffae00',
+                pointRadius : 0,
+                order: 4,
+                hoverRadius : 0,
+                hitRadius: 0
+            },
+            {   
+                label: 'yellow',
+                data: [4,4,4,4,4,4,4,4],
+                backgroundColor : '#FFFFFF00',
+                borderColor :'#FFFF33',
+                pointRadius : 0,
+                order: 3,
+                hoverRadius : 0,
+                hitRadius: 0
+            },
+            {   
+                data: [2,2,2,2,2,2,2,2],
+                backgroundColor : '#FFFFFF00',
+                borderColor :'#33CC33',
+                pointRadius : 0,
+                order:2,
+                hoverRadius : 0,
+                hitRadius: 0
+            },
+            {
+                label: 'Valore',
+                data: chart_data,
+                borderColor: '#000',
+                backgroundColor: '#a6a6a660',
+                pointBackgroundColor: function(context){
+                    var index = context.dataIndex;
+                    var value = context.dataset.data[index];
+                    return value <= 2 ? '#33CC33' : value <= 4 ? '#FFFF33' : value <= 6 ? '#ffae00' :'#FF3333';
+                }, 
+                pointRadius: 4,
+                order:1,
+            },
+            
+        ]
+    };
+
+    //Remvoes useless levels
+    if(max_value <= 6){
+      data.datasets.splice(0, 1)
+      if(max_value  <= 4){
+        data.datasets.splice(0, 1)
+      }
+    }
+
+    //chart Configuration    
+    const config = {
+        type: 'radar',
+        data: data,
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,    
+            scale:{
+                stepSize:1,
+                min : 0,
+                ticks:{
+                    beginAtZero: true,
+                    max: max_value,
+                    min: 0,
+                    stepSize: 1,
+                    display: false,
+                },
+            },
+            scales:{
+                r:{
+                    ticks:{
+                        display:false,
+                        max: max_value,
+                    },
+                },
+            },
+            plugins: {
+                title: {
+                display: false,
+                text: 'PSF'
+                },
+                legend: {
+                    display: false
+                },
+                tooltip:{
+                    callbacks:{
+                        label: function(context){
+                            var inverse_values_map = {
+                                1 : 0.1  ,
+                                2 : 1    ,
+                                3 : 2    ,
+                                4 : 5    ,
+                                5 : 10   ,
+                                6 : 15   ,
+                                7 : 20   ,
+                                8 : 50   ,
+                                9 : 100  
+                            } 
+                            return 'Valore: ' + inverse_values_map[context.raw];
+                        }
+                    }
+                },            
+            }   
+        },
+      };
+    
+    var canvas = document.getElementById("chart_PSF")
+    var PSF_chart = new Chart(canvas.getContext("2d"), config);  
+    canvas.addEventListener('click', function() {
+        //Apply white background
+        var a = document.createElement('a');
+        var context = canvas.getContext("2d");
+        context.save();
+        context.globalCompositeOperation = 'destination-over';
+        context.fillStyle = '#FFF';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.restore();
+
+        var image = PSF_chart.toBase64Image();
+        a.href = image;
+        a.download = 'PSF_chart.png';
+        a.click()
+     }, false);
+}
+
+//GENERATE THE CHART OF PSF IN RESUTS PAGE
+function generateChart_Barriere_dirette(values){
+    const DATA_COUNT = 8;
+    const chart_labels = ["Prestazione sicura rispetto ai compiti",
+                         "Adesione alle norme/procedure di sicurezza ",
+                         "Prestazione sicura orientata al contesto",
+                         "Partecipazione attiva alla sicurezza",
+                         "Lavoro di squadra",
+                         "Comunicazione inerente la sicurezza"
+                        ];
+    var values_list =  [values["PrestSicuraCompiti"],
+                        values["Adesione"],
+                        values["PrestSicuraContesto"],
+                        values["Partecipazione"],
+                        values["LavoroSquad"],
+                        values["Comunicazione"]]; 
 
     
     //To rescale the values that makes no sense elseway on the graph
