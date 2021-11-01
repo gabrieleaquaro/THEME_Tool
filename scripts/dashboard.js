e16c1d306f59c1c63f6183bb7d3a1c7d2637343f
gabrieleaquaro <gabriele.aquaro@gmail.com>
2021-11-01 19:47:23 +0100

    value in bars

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -343,6 +343,8 @@ function updateRisultati_results(report){
                 }
                 document.getElementById(key+"_bar").setAttribute("aria-valuenow", value * 100);
                 document.getElementById(key+"_bar").setAttribute("style","width: "+( value * 100)+"%");
+                document.getElementById(key+"_bar").style.color = '#000000';
+                document.getElementById(key+"_bar").innerText = ( value * 100)+"%";
                 total_valori = total_valori + value
             }
             else{
@@ -366,9 +368,12 @@ function updateRisultati_results(report){
         if(report['prob_errore']["ProbErroreAdj"] != undefined && report['prob_errore']["ProbErroreAdj"] != "-"){
             document.getElementById("ProbErroreAdj_bar").setAttribute("aria-valuenow", report['prob_errore']["ProbErroreAdj"] * 100);
             document.getElementById("ProbErroreAdj_bar").setAttribute("style","width: "+( report['prob_errore']["ProbErroreAdj"] * 100)+"%");
+            document.getElementById("ProbErroreAdj_bar").style.color = '#000000';
+            document.getElementById("ProbErroreAdj_bar").innerText = report['prob_errore']["ProbErroreAdj"] * 100 +"%";
         }else{
             document.getElementById("ProbErroreAdj_bar").setAttribute("aria-valuenow", 0);
             document.getElementById("ProbErroreAdj_bar").setAttribute("style","width: 0%");
+
         }
     }else{
         document.getElementById("ProbErroreAdj_bar").setAttribute("aria-valuenow", 0);
@@ -384,6 +389,7 @@ function updateRisultati_results(report){
             if(report['barriere_dirette'][key] == undefined || report['barriere_dirette'][key] == '-' ){
                 document.getElementById("average_barriere_dirette").setAttribute("aria-valuenow", 0);
                 document.getElementById("average_barriere_dirette").setAttribute("style","width: 0%");
+                
                 change = false
             }
             val += report['barriere_dirette'][key]
@@ -392,6 +398,8 @@ function updateRisultati_results(report){
             average_barriere_dirette = Math.round(val/6*100)/100
             document.getElementById("average_barriere_dirette").setAttribute("aria-valuenow", average_barriere_dirette * 100);
             document.getElementById("average_barriere_dirette").setAttribute("style","width: "+( average_barriere_dirette * 100)+"%");
+            document.getElementById("average_barriere_dirette").style.color = '#000000';
+            document.getElementById("average_barriere_dirette").innerText = (average_barriere_dirette * 100)+"%";
         }
     }else{
         average_barriere_dirette = 0
@@ -418,6 +426,8 @@ function updateRisultati_results(report){
             average_barriere_salvaguardia = Math.round(val/7*100)/100
             document.getElementById("average_barriere_salvaguardia").setAttribute("aria-valuenow", average_barriere_salvaguardia * 100);
             document.getElementById("average_barriere_salvaguardia").setAttribute("style","width: "+( average_barriere_salvaguardia * 100)+"%");
+            document.getElementById("average_barriere_salvaguardia").style.color = '#000000';
+            document.getElementById("average_barriere_salvaguardia").innerText = (average_barriere_salvaguardia * 100)+"%";
         }
     }else{
         average_barriere_salvaguardia = 0
@@ -428,6 +438,8 @@ function updateRisultati_results(report){
     if(average_barriere_salvaguardia + average_barriere_dirette > 0){
         document.getElementById("average_barriere").setAttribute("aria-valuenow", (average_barriere_salvaguardia + average_barriere_dirette)/2 * 100);
         document.getElementById("average_barriere").setAttribute("style","width: "+( (average_barriere_salvaguardia + average_barriere_dirette)/2 * 100)+"%");
+        document.getElementById("average_barriere").style.color = '#000000';
+        document.getElementById("average_barriere").innerText = Math.round(((average_barriere_salvaguardia + average_barriere_dirette)/2) * 100 * 100) / 100 +"%";
     }else{
         document.getElementById("average_barriere").setAttribute("aria-valuenow", 0);
         document.getElementById("average_barriere").setAttribute("style","width: 0%");
@@ -469,6 +481,9 @@ function updateRisultati_results(report){
 
             document.getElementById("ProbErroreAdj_barriere_valori").setAttribute("aria-valuenow", ProbErroreAdj_barriere_valori * 100);
             document.getElementById("ProbErroreAdj_barriere_valori").setAttribute("style","width: "+( ProbErroreAdj_barriere_valori * 100)+"%");
+            document.getElementById("ProbErroreAdj_barriere_valori").style.color = '#000000';
+            document.getElementById("ProbErroreAdj_barriere_valori").innerText = ProbErroreAdj_barriere_valori * 100+"%";    
+            
         }else{
             document.getElementById("ProbErroreAdj_barriere_valori").setAttribute("aria-valuenow", 0);
             document.getElementById("ProbErroreAdj_barriere_valori").setAttribute("style","width: 0%"); 
